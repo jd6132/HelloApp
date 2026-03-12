39a789d29b95fc08bd4b534dafcb8ef3124ebafd
jd6132 <jd6132@srmist.edu.in>
2026-03-12 13:39:36 +0530

    added

--- UC3.java
+++ UC3.java
@@ -1,10 +1,9 @@
 public class UC3 {
 	public static void main(String[] args) {
 		String name = "World";
-		if (args.length > 0) {
+		if(args.length > 0) {
 			name = args[0];
 		}
-		System.out.println("Hello, " + name + "!");
+	System.out.println("Hello, "+ name + "!");
 	}
 }
-
